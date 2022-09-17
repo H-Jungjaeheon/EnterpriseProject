655beeaebfd9420b54c3d003cc4e0f8644a2596c
H-JungJaeHeon <jjhking222@naver.com>
2022-09-18 02:06:06 +0900

    ScrollButtons&&UnderPanelMake
    ScrollButtons&&UnderPanelMake

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    [SerializeField]
+    protected bool isDontDestroyObj;
+
+    protected static T instance = null;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+                if (instance == null)
+                {
+                    instance = new GameObject().AddComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    // Start is called before the first frame update
+    protected void Awake()
+    {
+        if (isDontDestroyObj)
+        {
+            if (instance == null)
+            {
+                DontDestroyOnLoad(this.gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
