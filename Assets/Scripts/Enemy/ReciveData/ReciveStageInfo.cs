267db086fdeb10c4f53684679cbb1c60ed2a9641
sungbum05 <sblim05@naver.com>
2022-10-11 01:09:38 +0900

    InGameScene_LoadJsonData_100%

--- Assets/Scripts/Enemy/ReciveData/ReciveStageInfo.cs
+++ Assets/Scripts/Enemy/ReciveData/ReciveStageInfo.cs
@@ -1,13 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ReciveStageInfo : MonoBehaviour
 {
+    [Serializable]
+    public class Info
+    {
+        //스테이지 전체 정보
+        public int ID;
+        public int StageNumber;
+        public string IsBossStage;
+
+        //몬스터 스폰 수
+        public int Shrot_1;
+        public int Long_1;
+        public int Air_1;
+
+        public int Shrot_2;
+        public int Long_2;
+        public int Air_2;
+
+        public int Shrot_3;
+        public int Long_3;
+        public int Air_3;
+    }
+
+    [Serializable]
+    public class Infos
+    {
+        public Info[] StageData;
+    }
+
+    [SerializeField]
+    Infos DataList;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        string JsonData = Resources.Load<TextAsset>("JsonData/StageData").ToString();
+        Debug.Log(JsonData);
+
+        DataList = JsonUtility.FromJson<Infos>(JsonData);
     }
 
     // Update is called once per frame
