388e81d62d569a9719a9fc1c44cf9f19d2117159
sungbum05 <sblim05@naver.com>
2022-10-10 20:37:52 +0900

    InGameScene_JsonDataAdd_100%

---
+++ Assets/Scripts/Enemy/ReciveData/StageInfo.cs
@@ -0,0 +1,21 @@
+[System.Serializable]
+public class StageInfo
+{
+    //스테이지 전체 정보
+    public int ID;
+    public string StageNumber;
+    public bool IsBossStage;
+
+    //몬스터 스폰 수
+    public byte Shrot_1;
+    public byte Long_1;
+    public byte Air_1;
+
+    public byte Shrot_2;
+    public byte Long_2;
+    public byte Air_2;
+
+    public byte Shrot_3;
+    public byte Long_3;
+    public byte Air_3;
+}
