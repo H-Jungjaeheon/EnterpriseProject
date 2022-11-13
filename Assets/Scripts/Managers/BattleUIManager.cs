2f8a79395c83bc0e93641888f4bd158c9b0cbb49
H-JungJaeHeon <jjhking222@naver.com>
2022-11-13 23:33:38 +0900

    Fixed
    Fixed

--- Assets/Scripts/Managers/BattleUIManager.cs
+++ Assets/Scripts/Managers/BattleUIManager.cs
@@ -16,13 +16,6 @@ public enum Contents
     ContentsLength
 }
 
-public enum SaleOfFoodContents
-{
-    BasicScreen,
-    ChooseFoodScreen,
-    FoodMakingScreen
-}
-
 public enum UpgradeableBasicStats
 {
     Damage,
@@ -39,7 +32,6 @@ public class BattleUIManager : Singleton<BattleUIManager>
     #region 현재 상태 표기 관련 변수
     public Contents nowContents;
 
-    private SaleOfFoodContents nowSaleOfFoodContents;
     #endregion
 
     #region 콘텐츠 창 관련 변수들
@@ -51,20 +43,10 @@ public class BattleUIManager : Singleton<BattleUIManager>
 
     private GameObject lastContents; //전에 띄우던 콘텐츠 창 오브젝트
 
+    private int nowChangeContents; //바꿀 콘텐츠 창 인덱스
+
     [Tooltip("콘텐츠 창 오브젝트 모음")]
     public GameObject[] contentsPanelObjs;
-
-    [SerializeField]
-    [Tooltip("요리 판매 시스템 - 요리 선택 및 제작 창 오브젝트")]
-    private GameObject foodChooseAndMakePanelObj;
-
-    [SerializeField]
-    [Tooltip("요리 판매 시스템 - 요리 선택 창 오브젝트")]
-    private GameObject chooseADishObj;
-
-    [SerializeField]
-    [Tooltip("요리 판매 시스템 - 요리 제작 창 오브젝트")]
-    private GameObject theProductionObj;
 
     [SerializeField]
     [Tooltip("동료 시스템 - 동료 시스템 창 오브젝트")]
@@ -84,71 +66,6 @@ public class BattleUIManager : Singleton<BattleUIManager>
     private Text[] goodsTextRequiredForUpgrade;
     #endregion
 
-    #region 요리 판매 시스템 화면 텍스트, 변수, 오브젝트 모음
-    [Header("재료 개수 표기 텍스트(기본 화면)")]
-    [SerializeField]
-    [Tooltip("기본 화면의 모든 등급 개수 표기 텍스트들 모음")]
-    private Text[] materialsText_BasicScreen;
-
-    [Header("재료 개수 표기 텍스트(요리 선택 화면)")]
-    [SerializeField]
-    [Tooltip("요리 선택 화면의 모든 등급 개수 표기 텍스트들 모음")]
-    private Text[] materialsText_ChooseCookScreen;
-
-    [SerializeField]
-    [Tooltip("현재 제작할 요리 수량 표기 텍스트")]
-    private Text nowCookingCountText;
-
-    [SerializeField]
-    [Tooltip("현재 제작할 요리 이름 표기 텍스트")]
-    private Text nowCookingFoodNameText;
-
-    private int nowFoodIndex;
-    public int NowFoodIndex
-    {
-        get { return nowFoodIndex; }
-        set { nowFoodIndex = value; }
-    }
-
-    private int cookingCount; //현재 요리 개수
-
-    private int nowChangeContents; //바꿀 콘텐츠 창 인덱스
-
-    [Header("현재 요리에 필요한 재료 개수들")]
-    [SerializeField]
-    [Tooltip("현재 요리에 필요한 재료 개수")]
-    private int[] quantityOfMaterials;
-
-    private bool[] isMeetingTheNumberOfMaterials = new bool[3];
-
-    private bool isArrowMoving;
-
-    [HideInInspector]
-    public bool isCustomerArrival;
-
-    [SerializeField]
-    [Tooltip("손님 오브젝트")]
-    private GameObject customerObj;
-
-    [SerializeField]
-    [Tooltip("제작할 음식 오브젝트")]
-    private GameObject[] foodObjectsToCooking;
-
-    [SerializeField]
-    [Tooltip("미니게임 화살표 오브젝트")]
-    private GameObject arrowObj;
-
-    private Vector3 customerSpeed = new Vector3(1, 0, 0);
-
-    Color redTextColor = new Color(1, 0, 0);
-
-    Color greenTextColor = new Color(0, 1, 0.03f);
-
-    [SerializeField]
-    [Tooltip("각 음식들 필요 재료, 음식 이름")]
-    private FoodData[] foodDatas;
-    #endregion
-
     [SerializeField]
     [Tooltip("코인 재화 텍스트")]
     private Text coinText;
@@ -162,6 +79,11 @@ public class BattleUIManager : Singleton<BattleUIManager>
     private Text proficiencyText;
 
     [Header("그 외")]
+
+    [SerializeField]
+    [Tooltip("요리 판매 시스템 매니저 컴포넌트")]
+    private SaleOfFoodManager sofmComponent;
+    
     private BigInteger[] goodsRequiredForUpgrade = new BigInteger[(int)UpgradeableBasicStats.UpgradeableBasicStatsNumber];
 
     [SerializeField]
@@ -174,12 +96,10 @@ public class BattleUIManager : Singleton<BattleUIManager>
         StartSetting();
         BasicStatSetting();
         basicStatLevelText[(int)UpgradeableBasicStats.Damage].text = $"Lv {GameManager.Instance.statsLevel[(int)UpgradeableBasicStats.Damage]}";
-        StartCoroutine(customerOnTheWay());
     }
 
     void Update()
     {
-        SaleOfFoodViewMaterialsText_BasicScreen();
         TextSettings();
     }
 
@@ -194,17 +114,6 @@ public class BattleUIManager : Singleton<BattleUIManager>
     {
         gmInstance = GameManager.Instance;
 
-        cookingCount = 1;
-        NowFoodIndex = 0;
-
-        nowCookingFoodNameText.text = foodDatas[NowFoodIndex].FoodName;
-        nowCookingCountText.text = $"{cookingCount} 개";
-
-        for (int nowDataIndex = 0; nowDataIndex < quantityOfMaterials.Length; nowDataIndex++)
-        {
-            quantityOfMaterials[nowDataIndex] = foodDatas[NowFoodIndex].quantityOfMaterials[nowDataIndex];
-        }
-
         for (int nowIndex = 0; nowIndex < goodsRequiredForUpgrade.Length; nowIndex++)
         {
             string goodsRequiredForUpgradeString;
@@ -217,162 +126,6 @@ public class BattleUIManager : Singleton<BattleUIManager>
 
             goodsTextRequiredForUpgrade[nowIndex].text = $"강화\n{goodsRequiredForUpgradeString}원";
         }
-    }
-
-    private IEnumerator customerOnTheWay()
-    {
-        customerObj.transform.position = new Vector2(-3.5f, -3.2f);
-        while (customerObj.transform.position.x < -1)
-        {
-            customerObj.transform.position += customerSpeed * Time.deltaTime;
-            if (isCustomerArrival)
-            {
-                break;
-            }
-            yield return null;
-        }
-        isCustomerArrival = true;
-        customerObj.transform.position = new Vector2(-1, -3.15f);
-    }
-
-    private void SaleOfFoodViewMaterialsText_BasicScreen()
-    {
-        if (nowContents == Contents.SaleOfFoodContents)
-        {
-            var battleSceneManagerIn = BattleSceneManager.Instance;
-            for (int nowIndex = 0; nowIndex < 3; nowIndex++)
-            {
-                if (nowSaleOfFoodContents == SaleOfFoodContents.BasicScreen)
-                {
-                    materialsText_BasicScreen[nowIndex].text = $"{battleSceneManagerIn.quantityOfMaterials[nowIndex]} 개";
-                }
-                else if (nowSaleOfFoodContents == SaleOfFoodContents.ChooseFoodScreen)
-                {
-                    materialsText_ChooseCookScreen[nowIndex].text = $"{battleSceneManagerIn.quantityOfMaterials[nowIndex]} / {quantityOfMaterials[nowIndex] * cookingCount}";
-
-                    materialsText_ChooseCookScreen[nowIndex].color = (battleSceneManagerIn.quantityOfMaterials[nowIndex] < quantityOfMaterials[nowIndex] * cookingCount)
-                        ? materialsText_ChooseCookScreen[nowIndex].color = redTextColor : materialsText_ChooseCookScreen[nowIndex].color = greenTextColor;
-
-                    isMeetingTheNumberOfMaterials[nowIndex] = (battleSceneManagerIn.quantityOfMaterials[nowIndex] < quantityOfMaterials[nowIndex] * cookingCount)
-                        ? isMeetingTheNumberOfMaterials[nowIndex] = true : isMeetingTheNumberOfMaterials[nowIndex] = false;
-                }
-            }
-        }
-    }
-
-    public void ChangeTheFoodType(bool isChangeNextFoodType)
-    {
-        int maxFoodIndex = 2;
-        if (NowFoodIndex == maxFoodIndex && isChangeNextFoodType == false)
-        {
-            NowFoodIndex = 0;
-        }
-        else if (NowFoodIndex == 0 && isChangeNextFoodType)
-        {
-            NowFoodIndex = maxFoodIndex;
-        }
-        else
-        {
-            NowFoodIndex = isChangeNextFoodType ? NowFoodIndex - 1 : NowFoodIndex + 1;
-        }
-        ChangeFoodAnim(isChangeNextFoodType);
-        nowCookingFoodNameText.text = foodDatas[NowFoodIndex].FoodName;
-        for (int nowDataIndex = 0; nowDataIndex < quantityOfMaterials.Length; nowDataIndex++)
-        {
-            quantityOfMaterials[nowDataIndex] = foodDatas[NowFoodIndex].quantityOfMaterials[nowDataIndex];
-        }
-    }
-
-    private void ChangeFoodAnim(bool isChangeNextFoodType)
-    {
-        for (int nowIndex = 0; nowIndex < foodObjectsToCooking.Length; nowIndex++)
-        {
-            foodObjectsToCooking[nowIndex].GetComponent<FoodObj>().FoodMovingAnimStart(isChangeNextFoodType);
-        }
-    }
-
-    public void FoodChooseAndMakePanelOnOrOff(bool isPanelOn)
-    {
-        if (isPanelOn && isCustomerArrival == false)
-        {
-            return;
-        }
-        nowSaleOfFoodContents = isPanelOn ? SaleOfFoodContents.ChooseFoodScreen : SaleOfFoodContents.BasicScreen;
-        foodChooseAndMakePanelObj.SetActive(isPanelOn);
-    }
-
-    public void CookingPanelOn()
-    {
-        for (int nowIndex = 0; nowIndex < isMeetingTheNumberOfMaterials.Length; nowIndex++)
-        {
-            if (isMeetingTheNumberOfMaterials[nowIndex])
-            {
-                return;
-            }
-        }
-
-        for (int nowIndex = 0; nowIndex < isMeetingTheNumberOfMaterials.Length; nowIndex++)
-        {
-            BattleSceneManager.Instance.quantityOfMaterials[nowIndex] -= quantityOfMaterials[nowIndex] * cookingCount;
-        }
-
-        nowSaleOfFoodContents = SaleOfFoodContents.FoodMakingScreen;
-        chooseADishObj.SetActive(false);
-        theProductionObj.SetActive(true);
-        StartCoroutine(ArrowMiniGameStart());
-    }
-
-    public void StopArrow() => isArrowMoving = false;
-
-    IEnumerator ArrowMiniGameStart()
-    {
-        Vector2 arrowMoveSpeed = new Vector2(4f, 0);
-        bool isLeft = false;
-        isArrowMoving = true;
-
-        while (isArrowMoving)
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                break;
-            }
-            arrowObj.transform.position += isLeft ? (Vector3)arrowMoveSpeed * -Time.deltaTime : (Vector3)arrowMoveSpeed * Time.deltaTime;
-            if (isLeft && arrowObj.transform.position.x <= -1.5f)
-            {
-                isLeft = false;
-            }
-            else if (isLeft == false && arrowObj.transform.position.x >= 1.5f)
-            {
-                isLeft = true;
-            }
-            yield return null;
-        }
-        yield return new WaitForSeconds(5);
-
-        //원래는 실패하면 안줘야함
-
-        GameManager.Instance.CurrentProficiency += 10; //그냥 대충 올린 테스트 버전
-
-        theProductionObj.SetActive(false);
-        isCustomerArrival = false;
-        foodChooseAndMakePanelObj.SetActive(false);
-        chooseADishObj.SetActive(true);
-        nowSaleOfFoodContents = SaleOfFoodContents.BasicScreen;
-        nowContents = Contents.SaleOfFoodContents;
-        StartCoroutine(customerOnTheWay());
-    }
-
-    public void AdjustTheNumberOfFoods(bool isPlus)
-    {
-        if (isPlus == false && cookingCount > 1)
-        {
-            cookingCount--;
-        }
-        else if (isPlus && cookingCount < 99)
-        {
-            cookingCount++;
-        }
-        nowCookingCountText.text = $"{cookingCount} 개";
     }
 
     public void BasicStatSetting()
@@ -465,9 +218,9 @@ public class BattleUIManager : Singleton<BattleUIManager>
 
         nowContents = (Contents)nowChangeContents;
 
-        if (isCustomerArrival == false)
+        if (sofmComponent.isCustomerArrival == false)
         {
-            isCustomerArrival = true;
+            sofmComponent.isCustomerArrival = true;
         }
     }
 
@@ -519,9 +272,9 @@ public class BattleUIManager : Singleton<BattleUIManager>
         nowContents = isSameContents ? Contents.StatUpgradeContents : (Contents)nowChangeContents;
         nowContentsObj.SetActive(true);
 
-        if (isCustomerArrival == false)
+        if (sofmComponent.isCustomerArrival == false)
         {
-            isCustomerArrival = true;
+            sofmComponent.isCustomerArrival = true;
         }
     }
 
--- Assets/Scripts/Managers/SaleOfFoodManager.cs
+++ Assets/Scripts/Managers/SaleOfFoodManager.cs
@@ -1,18 +1,314 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public enum SaleOfFoodContents
+{
+    BasicScreen,
+    ChooseFoodScreen,
+    FoodMakingScreen
+}
 
 public class SaleOfFoodManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private SaleOfFoodContents nowSaleOfFoodContents;
+
+    [SerializeField]
+    [Tooltip("요리 판매 시스템 - 요리 선택 및 제작 창 오브젝트")]
+    private GameObject foodChooseAndMakePanelObj;
+
+    [SerializeField]
+    [Tooltip("요리 판매 시스템 - 요리 선택 창 오브젝트")]
+    private GameObject chooseADishObj;
+
+    [SerializeField]
+    [Tooltip("요리 판매 시스템 - 요리 제작 창 오브젝트")]
+    private GameObject theProductionObj;
+
+    #region 요리 판매 시스템 화면 텍스트, 변수, 오브젝트 모음
+    [Header("재료 개수 표기 텍스트(기본 화면)")]
+    [SerializeField]
+    [Tooltip("기본 화면의 모든 등급 개수 표기 텍스트들 모음")]
+    private Text[] materialsText_BasicScreen;
+
+    [Header("재료 개수 표기 텍스트(요리 선택 화면)")]
+    [SerializeField]
+    [Tooltip("요리 선택 화면의 모든 등급 개수 표기 텍스트들 모음")]
+    private Text[] materialsText_ChooseCookScreen;
+
+    [SerializeField]
+    [Tooltip("현재 제작할 요리 수량 표기 텍스트")]
+    private Text nowCookingCountText;
+
+    [SerializeField]
+    [Tooltip("현재 제작할 요리 이름 표기 텍스트")]
+    private Text nowCookingFoodNameText;
+
+    private int nowFoodIndex;
+    public int NowFoodIndex
+    {
+        get { return nowFoodIndex; }
+        set { nowFoodIndex = value; }
+    }
+
+    private int cookingCount; //현재 요리 개수
+
+    [Header("현재 요리에 필요한 재료 개수들")]
+    [SerializeField]
+    [Tooltip("현재 요리에 필요한 재료 개수")]
+    private int[] quantityOfMaterials;
+
+    private bool[] isMeetingTheNumberOfMaterials = new bool[3];
+
+    private bool isArrowMoving;
+
+    [HideInInspector]
+    public bool isCustomerArrival;
+
+    [SerializeField]
+    [Tooltip("손님 오브젝트")]
+    private GameObject customerObj;
+
+    [SerializeField]
+    [Tooltip("제작할 음식 오브젝트")]
+    private GameObject[] foodObjectsToCooking;
+
+    [SerializeField]
+    [Tooltip("미니게임 화살표 오브젝트")]
+    private GameObject arrowObj;
+
+    private Vector3 customerSpeed = new Vector3(1, 0, 0);
+
+    Color redTextColor = new Color(1, 0, 0);
+
+    Color greenTextColor = new Color(0, 1, 0.03f);
+
+    [SerializeField]
+    [Tooltip("각 음식들 필요 재료, 음식 이름")]
+    private FoodData[] foodDatas;
+    #endregion
+
+    #region 재료 투입 연출 요소 모음
+    [SerializeField]
+    [Tooltip("재료 리소스들")]
+    private Sprite[] ingredientSprite;
+
+    [SerializeField]
+    [Tooltip("재료 이미지")]
+    private Image ingredientImage;
+
+    [SerializeField]
+    [Tooltip("냄비 오브젝트")]
+    private GameObject potObj;
+
+    Color ingredientColor = new Color(1, 1, 1, 0);
+    #endregion
+
     void Start()
     {
-        
+        cookingCount = 1;
+        NowFoodIndex = 0;
+
+        nowCookingFoodNameText.text = foodDatas[NowFoodIndex].FoodName;
+        nowCookingCountText.text = $"{cookingCount} 개";
+
+        for (int nowDataIndex = 0; nowDataIndex < quantityOfMaterials.Length; nowDataIndex++)
+        {
+            quantityOfMaterials[nowDataIndex] = foodDatas[NowFoodIndex].quantityOfMaterials[nowDataIndex];
+        }
+
+        StartCoroutine(customerOnTheWay());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SaleOfFoodViewMaterialsText_BasicScreen();
+    }
+
+    private IEnumerator customerOnTheWay()
+    {
+        customerObj.transform.position = new Vector2(-3.5f, -3.2f);
+        while (customerObj.transform.position.x < -1)
+        {
+            customerObj.transform.position += customerSpeed * Time.deltaTime;
+            if (isCustomerArrival)
+            {
+                break;
+            }
+            yield return null;
+        }
+        isCustomerArrival = true;
+        customerObj.transform.position = new Vector2(-1, -3.15f);
+    }
+
+    private void SaleOfFoodViewMaterialsText_BasicScreen()
+    {
+        if (BattleUIManager.Instance.nowContents == Contents.SaleOfFoodContents)
+        {
+            var battleSceneManagerIn = BattleSceneManager.Instance;
+            for (int nowIndex = 0; nowIndex < 3; nowIndex++)
+            {
+                if (nowSaleOfFoodContents == SaleOfFoodContents.BasicScreen)
+                {
+                    materialsText_BasicScreen[nowIndex].text = $"{battleSceneManagerIn.quantityOfMaterials[nowIndex]} 개";
+                }
+                else if (nowSaleOfFoodContents == SaleOfFoodContents.ChooseFoodScreen)
+                {
+                    materialsText_ChooseCookScreen[nowIndex].text = $"{battleSceneManagerIn.quantityOfMaterials[nowIndex]} / {quantityOfMaterials[nowIndex] * cookingCount}";
+
+                    materialsText_ChooseCookScreen[nowIndex].color = (battleSceneManagerIn.quantityOfMaterials[nowIndex] < quantityOfMaterials[nowIndex] * cookingCount)
+                        ? materialsText_ChooseCookScreen[nowIndex].color = redTextColor : materialsText_ChooseCookScreen[nowIndex].color = greenTextColor;
+
+                    isMeetingTheNumberOfMaterials[nowIndex] = (battleSceneManagerIn.quantityOfMaterials[nowIndex] < quantityOfMaterials[nowIndex] * cookingCount)
+                        ? isMeetingTheNumberOfMaterials[nowIndex] = true : isMeetingTheNumberOfMaterials[nowIndex] = false;
+                }
+            }
+        }
+    }
+
+    public void ChangeTheFoodType(bool isChangeNextFoodType)
+    {
+        int maxFoodIndex = 2;
+        if (NowFoodIndex == maxFoodIndex && isChangeNextFoodType == false)
+        {
+            NowFoodIndex = 0;
+        }
+        else if (NowFoodIndex == 0 && isChangeNextFoodType)
+        {
+            NowFoodIndex = maxFoodIndex;
+        }
+        else
+        {
+            NowFoodIndex = isChangeNextFoodType ? NowFoodIndex - 1 : NowFoodIndex + 1;
+        }
+        ChangeFoodAnim(isChangeNextFoodType);
+        nowCookingFoodNameText.text = foodDatas[NowFoodIndex].FoodName;
+        for (int nowDataIndex = 0; nowDataIndex < quantityOfMaterials.Length; nowDataIndex++)
+        {
+            quantityOfMaterials[nowDataIndex] = foodDatas[NowFoodIndex].quantityOfMaterials[nowDataIndex];
+        }
+    }
+
+    private void ChangeFoodAnim(bool isChangeNextFoodType)
+    {
+        for (int nowIndex = 0; nowIndex < foodObjectsToCooking.Length; nowIndex++)
+        {
+            foodObjectsToCooking[nowIndex].GetComponent<FoodObj>().FoodMovingAnimStart(isChangeNextFoodType);
+        }
+    }
+
+    public void FoodChooseAndMakePanelOnOrOff(bool isPanelOn)
+    {
+        if (isPanelOn && isCustomerArrival == false)
+        {
+            return;
+        }
+        nowSaleOfFoodContents = isPanelOn ? SaleOfFoodContents.ChooseFoodScreen : SaleOfFoodContents.BasicScreen;
+        foodChooseAndMakePanelObj.SetActive(isPanelOn);
+    }
+
+    public void CookingPanelOn()
+    {
+        for (int nowIndex = 0; nowIndex < isMeetingTheNumberOfMaterials.Length; nowIndex++)
+        {
+            if (isMeetingTheNumberOfMaterials[nowIndex])
+            {
+                return;
+            }
+        }
+
+        for (int nowIndex = 0; nowIndex < isMeetingTheNumberOfMaterials.Length; nowIndex++)
+        {
+            BattleSceneManager.Instance.quantityOfMaterials[nowIndex] -= quantityOfMaterials[nowIndex] * cookingCount;
+        }
+
+        nowSaleOfFoodContents = SaleOfFoodContents.FoodMakingScreen;
+        chooseADishObj.SetActive(false);
+        theProductionObj.SetActive(true);
+        StartCoroutine(ArrowMiniGameStart());
+    }
+
+    public void StopArrow() => isArrowMoving = false;
+
+    IEnumerator ArrowMiniGameStart()
+    {
+        Vector2 arrowMoveSpeed = new Vector2(4f, 0);
+        bool isLeft = false;
+        int basicScore = 2;
+        isArrowMoving = true;
+
+        while (isArrowMoving)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                break;
+            }
+            arrowObj.transform.position += isLeft ? (Vector3)arrowMoveSpeed * -Time.deltaTime : (Vector3)arrowMoveSpeed * Time.deltaTime;
+            if (isLeft && arrowObj.transform.position.x <= -1.5f)
+            {
+                isLeft = false;
+            }
+            else if (isLeft == false && arrowObj.transform.position.x >= 1.5f)
+            {
+                isLeft = true;
+            }
+            yield return null;
+        }
+
+        ingredientImage.sprite = ingredientSprite[Random.Range(0, 4)];
+
+        for (int nowIndex = 0; nowIndex < 2; nowIndex++)
+        {
+            ingredientImage.transform.DOLocalMoveY(900, 0);
+            float nowAlpha = 0;
+            while (nowAlpha < 1)
+            {
+                nowAlpha += Time.deltaTime;
+                ingredientColor.a = nowAlpha;
+                ingredientImage.color = ingredientColor;
+                yield return null;
+            }
+
+            ingredientImage.transform.DOLocalMoveY(420, 0.5f).SetEase(Ease.InBack);
+
+            while (ingredientImage.transform.localPosition.y > 420)
+            {
+                yield return null;
+            }
+
+            potObj.transform.DOScale(new Vector3(1.08f, 1.08f, 1), 0.2f);
+            yield return new WaitForSeconds(0.2f);
+            potObj.transform.DOScale(new Vector3(1, 1, 1), 0.2f);
+            yield return new WaitForSeconds(0.7f);
+
+        }
+
+        yield return new WaitForSeconds(1);
+
+        GameManager.Instance.CurrentProficiency += basicScore;
+
+        theProductionObj.SetActive(false);
+        isCustomerArrival = false;
+        foodChooseAndMakePanelObj.SetActive(false);
+        chooseADishObj.SetActive(true);
+        nowSaleOfFoodContents = SaleOfFoodContents.BasicScreen;
+        BattleUIManager.Instance.nowContents = Contents.SaleOfFoodContents;
+        StartCoroutine(customerOnTheWay());
+    }
+
+    public void AdjustTheNumberOfFoods(bool isPlus)
+    {
+        if (isPlus == false && cookingCount > 1)
+        {
+            cookingCount--;
+        }
+        else if (isPlus && cookingCount < 99)
+        {
+            cookingCount++;
+        }
+        nowCookingCountText.text = $"{cookingCount} 개";
     }
 }
