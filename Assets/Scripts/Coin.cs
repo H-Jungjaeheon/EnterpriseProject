8c6bcd425a5b9779e01a52323e8d2388903f0b3d
sungbum05 <sblim05@naver.com>
2022-11-15 03:45:14 +0900

    InGameScene_Coin

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Coin : MonoBehaviour
+{
+    public int CoinValue;
+
+    public Vector3 GoToPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float Ran = Random.Range(0.3f, 0.5f);
+
+        this.transform.localScale = new Vector2(Ran, Ran);
+
+        StartCoroutine(Move());
+    }
+
+    IEnumerator Move()
+    {
+        yield return null;
+
+        float RanTime = Random.Range(0.5f, 0.7f);
+
+        this.transform.DOMove(this.transform.position + new Vector3(Random.Range(-0.6f, 0.6f), Random.Range(-0.6f, 0.6f), 0.0f), RanTime);
+        yield return new WaitForSeconds(RanTime + 0.3f);
+
+        this.transform.DOMove(GoToPos, RanTime);
+        yield return new WaitForSeconds(RanTime + 0.1f);
+
+        this.gameObject.GetComponent<SpriteRenderer>().DOFade(0.0f, 0.5f);
+        yield return new WaitForSeconds(0.5f);
+
+        yield break;
+    }
+}
