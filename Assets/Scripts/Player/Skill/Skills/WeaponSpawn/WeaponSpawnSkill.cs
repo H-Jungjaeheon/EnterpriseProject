7d782cc96d658429e23c08970e1d793ae2d92579
sungbum05 <sblim05@naver.com>
2022-11-15 01:20:12 +0900

    InGameScene_WeaponSkill

--- Assets/Scripts/Player/Skill/Skills/WeaponSpawn/WeaponSpawnSkill.cs
+++ Assets/Scripts/Player/Skill/Skills/WeaponSpawn/WeaponSpawnSkill.cs
@@ -28,27 +28,42 @@ public class WeaponSpawnSkill : Skill
 
         SpawnBase.transform.DOScale(new Vector3(0.5f, 0.5f, 0.5f), 0.5f);
         SpawnBaseSpriteRenderer.DOFade(1.0f, 0.5f);
+
         yield return new WaitForSeconds(0.5f);
 
-        while(CurShootCnt <= MaxShootCnt)
+        while (CurShootCnt <= MaxShootCnt)
         {
-            float RanTime = Random.Range(1.0f, 2.0f);
+            if (CurShootCnt % TikShootCnt == 0)
+            {
+                foreach (var Enemy in Player.Instance.Range.TargetEnemy)
+                {
+                    Enemy.GetComponent<Enemy>().StartTakeDamage(10, false);
+                }
+            }
 
-            GameObject Bullet = Instantiate(Bullets[Random.Range(0, Bullets.Count)], this.gameObject.transform.position, Quaternion.identity);
+            float RanTime = Random.Range(0.5f, 0.7f);
+
+            GameObject Bullet = Instantiate(Bullets[Random.Range(0, Bullets.Count)], this.gameObject.transform.position + new Vector3(0, Random.Range(-0.7f, 0.8f), 0), Quaternion.identity);
 
             Destroy(Bullet, RanTime);
 
-            Bullet.GetComponent<SpriteRenderer>().DOFade(0, RanTime);
-            Bullet.transform.DOMoveX(this.transform.position.x + Random.Range(2.0f, 3.0f), 1.0f);
+            Bullet.GetComponent<SpriteRenderer>().DOFade(0.2f, RanTime);
+            Bullet.transform.DOMoveX(this.transform.position.x + Random.Range(4.0f, 6.0f), RanTime);
 
             CurShootCnt++;
 
-            yield return new WaitForSeconds(Random.Range(0.2f, 0.4f));
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.2f));
         }
 
-        yield return new WaitForSeconds(SkillDuration);
+        SpawnBase.transform.DOScale(Vector3.zero, 0.5f);
+        SpawnBaseSpriteRenderer.DOFade(0, 0.5f);
+
+        yield return new WaitForSeconds(0.5f);
+
+        SpawnBase.SetActive(false);
         #endregion
 
+        CurShootCnt = 0;
         OffSkillEffect();
     }
 }
