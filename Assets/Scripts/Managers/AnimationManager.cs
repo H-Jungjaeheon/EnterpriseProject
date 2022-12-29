d5527abdd14c2ac3d3a2f89dbbb49608d248084f
sungbum05 <sblim05@naver.com>
2022-12-29 21:18:32 +0900

    AnimationManagerFunc

--- Assets/Scripts/Managers/AnimationManager.cs
+++ Assets/Scripts/Managers/AnimationManager.cs
@@ -9,6 +9,7 @@ public enum AnimationType
     Move, Attack, Hit, Any
 }
 
+[System.Serializable]
 public class ClipList
 {
     public AnimationType AnimationType;
@@ -23,9 +24,36 @@ public class AnimationManager : MonoBehaviour
 
     [Header("애니메이션 관린")]
     [SerializeField]
+    private Sprite OriginalSprite;
+    [SerializeField]
     private List<ClipList> ClipList;
     [SerializeField]
     private float AnimationSpeed;
     [SerializeField]
     private Dictionary<AnimationType, AnimationClip> AnimationList;
+    [SerializeField]
+    private Coroutine PlayAnimCorutine;
+
+    private void Start()
+    {
+        SpriteRenderer = this.GetComponent<SpriteRenderer>();
+
+        OriginalSprite = SpriteRenderer.sprite;
+    }
+
+    public void PlayAnimation(AnimationType Type, float Speed = 1.0f, bool Loop = false)
+    {
+        PlayAnimCorutine = StartCoroutine(PlayAnimationCorutine(Type, Speed, Loop));
+    }
+
+    public void StopAnimation()
+    {
+        StopCoroutine(PlayAnimCorutine);
+        SpriteRenderer.sprite = OriginalSprite;
+    }
+
+    IEnumerator PlayAnimationCorutine(AnimationType Type, float Speed = 1.0f, bool Loop = false)
+    {
+        yield return null;
+    }
 }
