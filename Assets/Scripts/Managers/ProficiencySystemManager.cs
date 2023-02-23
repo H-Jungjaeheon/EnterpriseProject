cab903e661755718d3241ee2ad54a1b6ef99ebef
H-JungJaeHeon <jjhking222@naver.com>
2023-02-23 20:57:49 +0900

    광고 시스템 리소스 적용, 퀘스트 수치들 조정, 스킨 시스템 버그 수정
    광고 시스템 리소스 적용, 퀘스트 수치들 조정, 스킨 시스템 버그 수정

--- Assets/Scripts/Managers/ProficiencySystemManager.cs
+++ Assets/Scripts/Managers/ProficiencySystemManager.cs
@@ -18,68 +18,89 @@ public enum TextColorKind
     Green
 }
 
+[System.Serializable]
+public class CharacterData
+{
+    [Tooltip("캐릭터 이름")]
+    public string name;
+
+    [Tooltip("캐릭터 버프 내용")]
+    public string buffContents;
+
+    [Tooltip("캐릭터 배경 포지션")]
+    public RectTransform bgAnchorPos;
+
+    [Tooltip("캐릭터 잠금 표시 오브젝트")]
+    public GameObject lockObj;
+
+    [Tooltip("캐릭터 잠금 해제 가격")]
+    public int unlockCost;
+
+    [Tooltip("캐릭터 잠금 해제 유무 판별")]
+    public bool isUnlock;
+
+    [Tooltip("캐릭터 장착 유무 판별")]
+    public bool isEquiping;
+}
+
 public class ProficiencySystemManager : Singleton<ProficiencySystemManager>
 {
-    [System.Serializable]
-    public class CharacterData
-    {
-        [Tooltip("캐릭터 이름")]
-        public string name;
-
-        [Tooltip("캐릭터 버프 내용")]
-        public string buffContents;
-
-        [Tooltip("캐릭터 배경 포지션")]
-        public RectTransform bgAnchorPos;
-
-        [Tooltip("캐릭터 잠금 표시 오브젝트")]
-        public GameObject lockObj;
-
-        [Tooltip("캐릭터 잠금 해제 가격")]
-        public int unlockCost;
-
-        [Tooltip("캐릭터 잠금 해제 유무 판별")]
-        public bool isUnlock; 
-
-        [Tooltip("캐릭터 장착 유무 판별")]
-        public bool isEquiping;
-    }
-
     [Tooltip("각 스킨 시스템 캐릭터 데이터들")]
     public CharacterData[] characterDatas;
 
     [SerializeField]
     [Tooltip("현재 캐릭터 이름 텍스트")]
-    private Text nameText;
+    Text nameText;
 
     [SerializeField]
     [Tooltip("현재 캐릭터 버프 내용 텍스트")]
-    private Text buffText;
+    Text buffText;
 
     [SerializeField]
     [Tooltip("현재 캐릭터 장착/잠금해제 버튼 안내 텍스트")]
-    private Text guideText;
+    Text guideText;
 
     [SerializeField]
     [Tooltip("현재 캐릭터 장착/잠금해제 버튼 가격 텍스트")]
-    private Text priceText;
+    Text priceText;
 
     [SerializeField]
     [Tooltip("업그레이드/잠금해제 버튼 재화 이미지")]
-    private Image goodsImage;
-
-    [SerializeField]
-    [Tooltip("텍스트 색 모음")]
-    private Color[] textColors;
+    Image goodsImage;
 
     [Tooltip("현재 캐릭터 스킨")]
     public CharacterKind nowKind;
 
     [Tooltip("드래그 시 마우스 포인트 시작 지점")]
-    private Vector3 dragStartMousePos;
+    Vector3 dragStartMousePos;
 
     [Tooltip("드래그 중인지 판별")]
-    private bool isDraging; 
+    bool isDraging;
+
+    [Tooltip("텍스트 포지션 세팅용 Vector3")]
+    Vector3 textPos = Vector3.zero;
+
+    [Tooltip("스킨 시스템에 쓰일 공통 딜레이")]
+    WaitForSeconds delay = new WaitForSeconds(0.5f);
+
+    #region 인스턴스 모음
+
+    #endregion
+
+
+    #region 다회 사용 문자열 모음
+    [Tooltip("GuideText : 스킨 잠금 해제 안내 문자열")]
+    const string SKIN_UNLOCK = "스킨 잠금해제";
+
+    [Tooltip("GuideText : 스킨 장착 가능 안내 문자열")]
+    const string SKIN_EQUIPABLE = "장착 가능";
+
+    [Tooltip("GuideText : 스킨 잠금 해제 안내 문자열")]
+    const string SKIN_EQUIPING = "장착중";
+
+    [Tooltip("빈 문자열")]
+    const string NULL = "";
+    #endregion
 
     private void OnEnable()
     {
@@ -123,6 +144,7 @@ public class ProficiencySystemManager : Singleton<ProficiencySystemManager>
                 isDraging = false;
                 break;
             }
+
             yield return null;
         }
     }
@@ -134,16 +156,19 @@ public class ProficiencySystemManager : Singleton<ProficiencySystemManager>
     /// <returns></returns>
     IEnumerator Draging(bool isRightDrag)
     {
-        int imageTargetPosX;
+        Vector2 bgPos = Vector2.zero;
+        int imageTargetPosX = isRightDrag ? 1300 : -1300; //오른쪽으로 드래그 했다면 현재 이미지 이동 목표 X값 1300으로 설정(왼쪽으로 드래그 시 -1300)
 
-        imageTargetPosX = isRightDrag ? 1300 : -1300; //오른쪽으로 드래그 했다면 현재 이미지 이동 목표 X값 1300으로 설정(왼쪽으로 드래그 시 -1300)
+        bgPos.x = imageTargetPosX;
 
         isDraging = true;
-        characterDatas[(int)nowKind].bgAnchorPos.DOAnchorPos(new Vector2(imageTargetPosX, 0), 0.3f);
 
-        if (isRightDrag && nowKind == CharacterKind.Basic || isRightDrag == false && nowKind == CharacterKind.FourthCharacter) //
+        characterDatas[(int)nowKind].bgAnchorPos.DOAnchorPos(bgPos, 0.3f);
+
+        if (isRightDrag && nowKind == CharacterKind.Basic || isRightDrag == false && nowKind == CharacterKind.FourthCharacter)
         {
             StartCoroutine(ImagePosInitialization(-imageTargetPosX, isRightDrag));
+
             yield break;
         }
         else
@@ -151,11 +176,11 @@ public class ProficiencySystemManager : Singleton<ProficiencySystemManager>
             nowKind = isRightDrag ? nowKind - 1 : nowKind + 1;
         }
 
-        characterDatas[(int)nowKind].bgAnchorPos.DOAnchorPos(new Vector2(0, 0), 0.3f);
+        characterDatas[(int)nowKind].bgAnchorPos.DOAnchorPos(Vector2.zero, 0.3f);
 
         TextReSettings();
 
-        yield return new WaitForSeconds(0.035f);
+        yield return delay;
         
         isDraging = false;
     }
@@ -168,23 +193,27 @@ public class ProficiencySystemManager : Singleton<ProficiencySystemManager>
     /// <returns></returns>
     IEnumerator ImagePosInitialization(int targetPos, bool isRightDrag)
     {
+        Vector2 curTargetPos = Vector2.zero;
+        curTargetPos.x = targetPos;
+
         for (int nowIndex = 0; nowIndex <= (int)CharacterKind.FourthCharacter; nowIndex++) //전체 이미지 위치 초기화(현재 이동중인 이미지는 딜레이 후 위치 변경)
         {
             if (isRightDrag == false && nowIndex == (int)CharacterKind.FourthCharacter || isRightDrag && nowIndex == (int)CharacterKind.Basic)
             {
-                StartCoroutine(ImagerelocationDelay(new Vector2(targetPos, 0), nowIndex, true));
+                StartCoroutine(ImagerelocationDelay(curTargetPos, nowIndex, true));
             }
             else
             {
-                StartCoroutine(ImagerelocationDelay(new Vector2(targetPos, 0), nowIndex, false));
+                StartCoroutine(ImagerelocationDelay(curTargetPos, nowIndex, false));
             }
         }
 
         nowKind = isRightDrag ? CharacterKind.FourthCharacter : CharacterKind.Basic;
 
-        characterDatas[(int)nowKind].bgAnchorPos.DOAnchorPos(new Vector2(0, 0), 0.3f);
+        characterDatas[(int)nowKind].bgAnchorPos.DOAnchorPos(Vector2.zero, 0.3f);
 
         TextReSettings();
+
         yield return null;
     }
 
@@ -199,9 +228,11 @@ public class ProficiencySystemManager : Singleton<ProficiencySystemManager>
     {
         if (isGiveDelay)
         {
-            yield return new WaitForSeconds(0.35f);
+            yield return delay;
+
             isDraging = false;
         }
+
         characterDatas[moveImageIndex].bgAnchorPos.DOAnchorPos(targetPos, 0);
     }
 
@@ -217,20 +248,18 @@ public class ProficiencySystemManager : Singleton<ProficiencySystemManager>
             characterDatas[(int)nowKind].isUnlock = true;
 
             characterDatas[(int)nowKind].lockObj.SetActive(false);
-
-            TextReSettings();
         }
         else if(characterDatas[(int)nowKind].isUnlock && characterDatas[(int)nowKind].isEquiping == false)
         {
             for (int nowIndex = (int)CharacterKind.Basic; nowIndex <= (int)CharacterKind.FourthCharacter; nowIndex++) //다른 인덱스 캐릭터들은 장착 해제, 현재 인덱스 캐릭터만 장착으로 갱신
             {
-                characterDatas[(int)nowKind].isEquiping = (nowIndex == (int)nowKind) ? true : false;
+                characterDatas[nowIndex].isEquiping = (nowIndex == (int)nowKind) ? true : false;
             }
 
             Player.Instance.CharacterChange((int)nowKind);
-
-            TextReSettings();
         }
+
+        TextReSettings();
     }
 
     /// <summary>
@@ -244,28 +273,27 @@ public class ProficiencySystemManager : Singleton<ProficiencySystemManager>
 
         if (characterDatas[(int)nowKind].isUnlock == false)
         {
-            guideText.color = (characterDatas[(int)nowKind].unlockCost <= GameManager.Instance.GemUnit) ? textColors[(int)TextColorKind.Green] : textColors[(int)TextColorKind.Red];
+            guideText.color = (characterDatas[(int)nowKind].unlockCost <= GameManager.Instance.GemUnit) ? Color.green : Color.red;
+            goodsImage.color = Color.white;
 
-            guideText.transform.localPosition = new Vector3(0, 185, 0);
+            textPos.y = 185f;
+            guideText.transform.localPosition = textPos;
 
-            guideText.text = "캐릭터 잠금해제";
+            guideText.text = SKIN_UNLOCK;
             priceText.text = $"{characterDatas[(int)nowKind].unlockCost}";
 
             goodsImage.GetComponent<RectTransform>().anchoredPosition = new Vector3(-70 + ((priceText.text.Length - 1) * -20), 15, 0); //필요 재화 단위에 따라서 이미지 위치 변경
-
-            goodsImage.color = new Color(1, 1, 1, 1);
         }
         else
         {
-            guideText.color = textColors[(int)TextColorKind.Black];
+            guideText.color = Color.black;
+            goodsImage.color = Color.clear;
 
-            goodsImage.color = new Color(1, 1, 1, 0);
+            textPos.y = 140f;
+            guideText.transform.localPosition = textPos;
             
-            guideText.transform.localPosition = new Vector3(0, 140, 0);
-            
-            priceText.text = "";
-
-            guideText.text = characterDatas[(int)nowKind].isEquiping == false ? guideText.text = "장착 가능" : guideText.text = "장착중";
+            priceText.text = NULL;
+            guideText.text = characterDatas[(int)nowKind].isEquiping == false ? guideText.text = SKIN_EQUIPABLE : guideText.text = SKIN_EQUIPING;
         }
     }
 }
